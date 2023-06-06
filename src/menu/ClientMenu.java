bb4a7577984d35104e9138d9e313d466136f5f36
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-07 02:03:34 +0300

    Create selectAccountGroup

--- src/menu/ClientMenu.java
+++ src/menu/ClientMenu.java
@@ -1,5 +1,7 @@
  package menu;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import account.*;
@@ -58,72 +60,115 @@ public class ClientMenu{
 		client.getAccountGroup().display();
 		
 	}
+    
+    private AccountGroup selectAccountGroup() {
+    	//display all the account group types and select one
+    	List<AbstractAccount> accountList =  client.getAccountGroup().getAccounts();
+    	List<AccountGroup> accountGroupList = new ArrayList<AccountGroup>();
+    	System.out.println("Select an Account Group:");
+    	System.out.println("1. +AccountGroup: Top Account Group");
+    	accountGroupList.add(client.getAccountGroup());
+    	int count = 2;
+    	for(int i = 0; i< accountList.size(); i++) {
+    		AbstractAccount currentAccount = accountList.get(i);
+    		if(currentAccount instanceof AccountGroup) {
+    			System.out.print(count + ". ");
+    			currentAccount.display();
+    			accountGroupList.add((AccountGroup) currentAccount);
+    			count ++;
+    		}
+    	}
+    	int selection = helperIO.readIntegerInput();
+    	if (selection >= 1 && selection <= accountGroupList.size()) {
+    		return accountGroupList.get(selection-1);
+    	}
+    	else {
+    		throw new IllegalArgumentException("Invalid account group selection. Please try again.");
+    	}
+    }
 
 	private void createAccount() {
         AccountFactoryFactory accountFactoryFactory = new AccountFactoryFactory();
         AbstractAccount newAccount = null;
-        System.out.println("Enter the account type: ");
-        helperMenu.printAccountTypeMenu();
-        int type = helperIO.readIntegerInput();
-        if (type >= 1 && type <= 4) {
-        	//create the account factory according to the selection
-        	AccountFactory selectedAccountFactory = accountFactoryFactory.createAccountFactory(MainAccountType.getNameByValue(type));
-        	if(type == 1) {
-        		//regular menu
-        		helperMenu.printRegularAccountMenu();
-        		int type1 = helperIO.readIntegerInput();
-        		if(RegularAccountType.isValue(type1)) {
-            		newAccount = selectedAccountFactory.createAccount(RegularAccountType.getNameByValue(type1));
-        		}
-        		else {
-        			System.out.println("Invalid account type. Please try again.");
-        		}
+        AccountGroup selectedGroup = null;
+        
+        //First select an account group to add
+        try {
+            selectedGroup = selectAccountGroup();
+            System.out.println("Your account will be created under: "); 
+            selectedGroup.display();
+            System.out.println("Enter the account type: ");
+            helperMenu.printAccountTypeMenu();
+            int type = helperIO.readIntegerInput();
+            if (type >= 1 && type <= 4) {
+            	//create the account factory according to the selection
+            	AccountFactory selectedAccountFactory = accountFactoryFactory.createAccountFactory(MainAccountType.getNameByValue(type));
+            	if(type == 1) {
+            		//regular menu
+            		helperMenu.printRegularAccountMenu();
+            		int type1 = helperIO.readIntegerInput();
+            		if(RegularAccountType.isValue(type1)) {
+                		newAccount = selectedAccountFactory.createAccount(RegularAccountType.getNameByValue(type1));
+            		}
+            		else {
+            			System.out.println("Invalid account type. Please try again.");
+            		}
 
-        	}
-        	else if(type == 2) {
-        		//foreign currency menu
-        		helperMenu.printForeignCurrencyAccountMenu();
-        		int type2 = helperIO.readIntegerInput();
-				if (ForeignCurrencyAccountType.isValue(type2)) {
-				    newAccount = selectedAccountFactory.createAccount(ForeignCurrencyAccountType.getNameByValue(type2));
-				} else {
-				    System.out.println("Invalid account type. Please try again.");
-				}
-			}
-        	
-        	else if(type == 3) {
-        		//gold menu
-        		helperMenu.printGoldAccountMenu();
-        		int type3 = helperIO.readIntegerInput();
-        		if (GoldAccountType.isValue(type3)) {
-        		    newAccount = selectedAccountFactory.createAccount(GoldAccountType.getNameByValue(type3));
-        		} else {
-        		    System.out.println("Invalid account type. Please try again.");
-        		}
-        	}
-        	else if(type == 4) {
-        		//investment menu
-        		newAccount = selectedAccountFactory.createAccount("investment");
-        	}  
-        	else {
-        		 System.out.println("Invalid account type. Please try again.");
-        	}
-        } else {
-            System.out.println("Invalid account type. Please try again.");
+            	}
+            	else if(type == 2) {
+            		//foreign currency menu
+            		helperMenu.printForeignCurrencyAccountMenu();
+            		int type2 = helperIO.readIntegerInput();
+    				if (ForeignCurrencyAccountType.isValue(type2)) {
+    				    newAccount = selectedAccountFactory.createAccount(ForeignCurrencyAccountType.getNameByValue(type2));
+    				} else {
+    				    System.out.println("Invalid account type. Please try again.");
+    				}
+    			}
+            	
+            	else if(type == 3) {
+            		//gold menu
+            		helperMenu.printGoldAccountMenu();
+            		int type3 = helperIO.readIntegerInput();
+            		if (GoldAccountType.isValue(type3)) {
+            		    newAccount = selectedAccountFactory.createAccount(GoldAccountType.getNameByValue(type3));
+            		} else {
+            		    System.out.println("Invalid account type. Please try again.");
+            		}
+            	}
+            	else if(type == 4) {
+            		//investment menu
+            		newAccount = selectedAccountFactory.createAccount("investment");
+            	}  
+            	else {
+            		 System.out.println("Invalid account type. Please try again.");
+            	}
+            } else {
+                System.out.println("Invalid account type. Please try again.");
+            }
+            if (newAccount != null) {
+            	if(selectedGroup.equals(client.getAccountGroup())) {
+            		client.addToTopAccountGroup(newAccount);
+            	}
+            	else {
+            		client.addToGivenAccountGroup(newAccount, selectedGroup);
+            	}
+            	
+            	System.out.println("New account created successfully.");
+            	System.out.println("newAccount" + newAccount.toString());
+            }
+            System.out.println();
+            
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        if (newAccount != null) {
-        	client.addToAccounts(newAccount);
-        	System.out.println("New account created successfully.");
-        	System.out.println("newAccount" + newAccount.toString());
-        }
-        System.out.println();
     }
 
     public void createAccountGroup() {
         System.out.print("Enter the name of the account group: ");
         String groupName = scanner.nextLine();
         AccountGroup accountGroup = new AccountGroup(groupName);
-        client.addToAccounts(accountGroup);
+        client.addToTopAccountGroup(accountGroup);
         System.out.println("Account group created successfully.");
         System.out.println("newAccount group" + accountGroup.toString());
         System.out.println();

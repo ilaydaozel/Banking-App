667a4d8b3054a4f18f78d82a927907206dee8029
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-07 23:08:14 +0300

    Create ForeignCurrencyAccountEURWithoutInterestMenu.java

---
+++ src/menu/ForeignCurrencyAccountEURWithoutInterestMenu.java
@@ -0,0 +1,29 @@
+package menu;
+import java.util.Scanner;
+
+public class ForeignCurrencyAccountEURWithoutInterestMenu {
+    public static void displayMenu() {
+        System.out.println("------ Foreign Currency Account (EUR) without Interest Menu ------");
+        System.out.println("1. Exchange to TRY Account");
+        System.out.println("0. Go back to main menu");
+
+        System.out.print("Enter your choice: ");
+    }
+
+    public static void handleChoice() {
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                // Logic to exchange to a TRY account
+                System.out.println("Exchanging to a TRY account...");
+                break;
+            case 0:
+                // Go back to the main menu
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+        }
+    }
+}

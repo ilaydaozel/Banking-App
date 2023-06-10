c6aeee02993cd02fc85169aaa38456ea1a8bac3a
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-10 22:09:56 +0300

    Update XAU.java

--- src/currency/XAU.java
+++ src/currency/XAU.java
@@ -2,10 +2,12 @@ package currency;
 
 public class XAU extends Currency {
 
-	public XAU(double rate) {
-		super(rate);
+
+	public XAU(double rate, double dailyInterestRate) {
+		super(rate, dailyInterestRate);
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public String toString() {
 		return "XAU";

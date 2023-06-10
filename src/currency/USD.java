097f0817524bf6a6648a9fc3c3940354c32a45db
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-10 22:09:59 +0300

    Update USD.java

--- src/currency/USD.java
+++ src/currency/USD.java
@@ -2,10 +2,12 @@ package currency;
 
 public class USD extends Currency{
 
-	public USD(double rate) {
-		super(rate);
+
+	public USD(double rate, double dailyInterestRate) {
+		super(rate, dailyInterestRate);
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	public String toString() {
 		return "USD";

09abf2dddca6dc45958459e8807835b6a9960948
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-10 17:36:37 +0300

    Update HelperMenu.java

--- src/helpers/HelperMenu.java
+++ src/helpers/HelperMenu.java
@@ -167,6 +167,7 @@ public class HelperMenu {
         System.out.println("------ Investment Account Menu ------");
         System.out.println("1. Buy Stocks");
         System.out.println("2. Buy Funds");
+        System.out.println("3. Display Stocks and Funds");
         System.out.println("0. Go back to main menu");
 
         System.out.print("Enter your choice: ");

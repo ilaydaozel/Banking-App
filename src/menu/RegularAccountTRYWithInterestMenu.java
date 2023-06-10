bd926df76f71a15029d18284988f544972858619
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-10 19:36:10 +0300

    Update RegularAccountTRYWithInterestMenu.java

--- src/menu/RegularAccountTRYWithInterestMenu.java
+++ src/menu/RegularAccountTRYWithInterestMenu.java
@@ -2,15 +2,20 @@ package menu;
 
 import java.util.Scanner;
 
+import account.AbstractAccount;
 import account.RegularAccountWithInterest;
+import helpers.HelperClient;
 import helpers.HelperIO;
 import helpers.HelperMenu;
+import user.Client;
 
 public class RegularAccountTRYWithInterestMenu  {
-    HelperIO helperIO = new HelperIO();
-    HelperMenu helperMenu = new HelperMenu();
 
-    public void RegularAccountTRYWithInterestMenu(RegularAccountWithInterest account) {
+    public RegularAccountTRYWithInterestMenu(RegularAccountWithInterest account, Client client) {
+    	HelperClient helperClient = new HelperClient(client);
+        HelperIO helperIO = new HelperIO();
+        HelperMenu helperMenu = new HelperMenu();
+        
         boolean exit = false;
         while (!exit) {
             helperMenu.printRegularAccountTRYWithInterestMenu ();
@@ -19,6 +24,13 @@ public class RegularAccountTRYWithInterestMenu  {
             switch (choice) {
                 case 1:
                 	System.out.println("Balance:" + account.getBalance());
+                    break;
+                case 2:
+                	System.out.println("Select a regular account without interest to make exchange to:");
+                	AbstractAccount destAccount = helperClient.selectAnAccount();
+                    System.out.println("Enter exchange amount (TRY):");
+                    double exchangeAmount = helperIO.readDoubleInput();
+                	account.exchange(destAccount, exchangeAmount);
                     break;
                 case 0:
                     // Go back to the main menu

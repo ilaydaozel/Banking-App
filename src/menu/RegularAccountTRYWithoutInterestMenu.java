260f9af5b4df486e171df30589244b296db86587
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-10 19:32:02 +0300

    Update RegularAccountTRYWithoutInterestMenu.java

--- src/menu/RegularAccountTRYWithoutInterestMenu.java
+++ src/menu/RegularAccountTRYWithoutInterestMenu.java
@@ -2,19 +2,23 @@ package menu;
 
 import java.util.Scanner;
 
+import account.AbstractAccount;
+import account.ForeignCurrencyAccountWithoutInterest;
 import account.RegularAccountWithInterest;
 import account.RegularAccountWithoutInterest;
+import helpers.HelperClient;
 import helpers.HelperIO;
 import helpers.HelperMenu;
 import user.Client;
 
 public class RegularAccountTRYWithoutInterestMenu {
-    private static Scanner scanner = new Scanner(System.in);
 
-    HelperIO helperIO = new HelperIO();
-    HelperMenu helperMenu = new HelperMenu();
-
-    public void displayRegularAccountTRYWithoutInterestMenu(RegularAccountWithoutInterest account) {
+    public RegularAccountTRYWithoutInterestMenu(RegularAccountWithoutInterest account, Client client) {
+    	Scanner scanner = new Scanner(System.in);
+    	HelperClient helperClient = new HelperClient(client);
+        HelperIO helperIO = new HelperIO();
+        HelperMenu helperMenu = new HelperMenu();
+        
         boolean exit = false;
         while (!exit) {
             helperMenu.printRegularAccountTRYWithoutInterestMenu();
@@ -33,6 +37,13 @@ public class RegularAccountTRYWithoutInterestMenu {
                     account.deposit(amount);
                     System.out.println("New balance:"+ account.getBalance());                   
                     break;
+                case 3:
+                	System.out.println("Select a regular account without interest to make exchange to:");
+                	AbstractAccount destAccount = helperClient.selectAnAccount();
+                    System.out.println("Enter exchange amount (TRY):");
+                    double exchangeAmount = helperIO.readDoubleInput();
+                	account.exchange(destAccount, choice);
+                    break;
                 case 0:
                     // Go back to the main menu
                     exit = true;
@@ -42,6 +53,6 @@ public class RegularAccountTRYWithoutInterestMenu {
             }
         }
     }
+    
 }
 
-// Similar classes for other account menus

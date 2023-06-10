4c73cc889d8bd360460caf42d5c62f40d2a8ac43
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-10 23:44:30 +0300

    Refactor main

--- src/application/App.java
+++ src/application/App.java
@@ -11,51 +11,10 @@ import user.Client;
 import helpers.*;
 
 public class App {
-    private static Bank bank= Bank.getInstance();;
-    private static MainMenu mainMenu;
-    private static ClientMenu clientMenu;
-    private static BankMenu bankMenu;
-    private static Client currentClient;
-    
+    private static Bank bank= Bank.getInstance();
+
 	public static void main(String[] args) {
-        mainMenu = new MainMenu(bank);
-        bankMenu = new BankMenu(bank);
-        HelperIO helperIO = new HelperIO();
-        
-        boolean exit = false;
-        while (!exit) {
-            mainMenu.printMenu();
-            int choice = helperIO.readIntegerInput();
-
-            switch (choice) {
-            case 1:
-                mainMenu.createClient();
-                break;
-            case 2:
-            	Client client = mainMenu.selectClient();
-            	if( client != null) {
-            		currentClient = client;
-            		clientMenu = new ClientMenu(currentClient);
-            		clientMenu.clientMenu();
-            	}
-            	break;
-            case 3:
-            	bankMenu.bankMenu();
-            	break;
-            case 4:
-            	exit =true;
-            	System.out.println("You exit the app!");
-            	break;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-                System.out.println();
-                break;
-	        }
-	    }
-            	
-        }
-
-
-    
-
+		MainMenu mainMenu = new MainMenu(bank);
+        mainMenu.performMainMenu();
+    }
 }

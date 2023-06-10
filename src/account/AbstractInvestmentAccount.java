a58bba945a6dd0856f6a3f758332a904ddcbf08e
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-10 15:27:19 +0300

    Update AbstractInvestmentAccount.java

--- src/account/AbstractInvestmentAccount.java
+++ src/account/AbstractInvestmentAccount.java
@@ -1,8 +1,16 @@
 package account;
 
+import java.util.List;
+
+import enums.CurrencyType;
+import investment.Commodity;
+
 public abstract class AbstractInvestmentAccount extends AbstractAccount {
 	private int id;
+	private List <Commodity> commodities;
+	
 	public AbstractInvestmentAccount(int id) {
+		super.setCurrencyType(CurrencyType.TRY);
 		this.setId(id);
 		// TODO Auto-generated constructor stub
 	}
@@ -12,5 +20,14 @@ public abstract class AbstractInvestmentAccount extends AbstractAccount {
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	public List <Commodity> getCommodities() {
+		return commodities;
+	}
+	public void setCommodities(List <Commodity> assets) {
+		this.commodities = assets;
+	}
+	public abstract void buyCommodities();
+		
+	
+	
 }

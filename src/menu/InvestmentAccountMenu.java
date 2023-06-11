63ca189619bf072a4fcbedd85565f199375285c0
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 11:32:55 +0300

    Implement IMenu to investment menu

--- src/menu/InvestmentAccountMenu.java
+++ src/menu/InvestmentAccountMenu.java
@@ -1,42 +1,54 @@
 package menu;
 
-import java.util.List;
 import java.util.Scanner;
 
 import account.InvestmentAccount;
-import helpers.*;
+import helpers.HelperBank;
+import helpers.HelperIO;
+import interfaces.IMenu;
 import investment.Fund;
 import investment.Stock;
 
-public class InvestmentAccountMenu  {
+public class InvestmentAccountMenu implements IMenu {
 
-    public InvestmentAccountMenu (InvestmentAccount investmentAccount) {
+    private InvestmentAccount investmentAccount;
+
+    public InvestmentAccountMenu(InvestmentAccount investmentAccount) {
+        this.investmentAccount = investmentAccount;
+    }
+
+    @Override
+    public void displayMenu() {
+        System.out.println("------ Investment Account Menu ------");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Buy Stocks");
+        System.out.println("3. Buy Funds");
+        System.out.println("4. Display Stocks and Funds");
+        System.out.println("0. Go back to main menu");
+    }
+
+    @Override
+    public void handleChoice(int choice) {
         Scanner scanner = new Scanner(System.in);
         HelperIO helperIO = new HelperIO();
         HelperBank helperBank = new HelperBank(investmentAccount.getBank());
 
-        boolean exit = false;
-        while (!exit) {
-            printInvestmentAccountMenu();
-            int choice = helperIO.readIntegerInput();
-
-            switch (choice) {
+        switch (choice) {
             case 1:
-            	System.out.println("Balance:" + investmentAccount.getBalance());
-            	System.out.println();
-            	break;
+                System.out.println("Balance: " + investmentAccount.getBalance());
+                System.out.println();
+                break;
             case 2:
-	       		helperBank.displayStocks();
-	 		    // Get user input
-	 		    System.out.print("Enter the name of the stock you want to buy: ");
-	 		    String stockName = scanner.next();
-	 		    Stock stock = investmentAccount.getBank().getStock(stockName);
-	 		    investmentAccount.buyCommodity(stock);
-	 		    System.out.println();
-	                
-	            break;
+                helperBank.displayStocks();
+                // Get user input
+                System.out.print("Enter the name of the stock you want to buy: ");
+                String stockName = scanner.next();
+                Stock stock = investmentAccount.getBank().getStock(stockName);
+                investmentAccount.buyCommodity(stock);
+                System.out.println();
+                break;
             case 3:
-            	helperBank.displayFunds();
+                helperBank.displayFunds();
                 // Get user input
                 System.out.print("Enter the name of the fund you want to buy: ");
                 String fundName = scanner.next();
@@ -47,33 +59,22 @@ public class InvestmentAccountMenu  {
                 System.out.println();
                 break;
             case 4:
-            	investmentAccount.displayCommodities();
-                // Go back to the main menu
+                investmentAccount.displayCommodities();
                 break;
             case 0:
-            	exit = true;
-            	
                 // Go back to the main menu
                 break;
             default:
                 System.out.println("Invalid choice. Please try again.");
         }
-
-        }
     }
-    
 
-    
-    private void printInvestmentAccountMenu() {
-        System.out.println("------ Investment Account Menu ------");
-        System.out.println("1. Check Balance");
-        System.out.println("2. Buy Stocks");
-        System.out.println("3. Buy Funds");
-        System.out.println("4. Display Stocks and Funds");
-        System.out.println("0. Go back to main menu");
-
-        System.out.print("Enter your choice: ");
+    public void executeMenu() {
+        displayMenu();
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        handleChoice(choice);
     }
-    
 }
+
 

30eb249c67da1ef4ffcbf9a9f958e4c8066afde1
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 14:27:02 +0300

    Update GoldAccountWithoutInterest.java

--- src/account/GoldAccountWithoutInterest.java
+++ src/account/GoldAccountWithoutInterest.java
@@ -6,7 +6,6 @@ public class GoldAccountWithoutInterest extends AbstractGoldAccount implements I
 
 	public GoldAccountWithoutInterest(int id) {
 		super(id);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
@@ -14,28 +13,36 @@ public class GoldAccountWithoutInterest extends AbstractGoldAccount implements I
 		System.out.println("- id:" + super.getId() +" Gold Account Without Interest");
 		
 	}
-	@Override
-	public void exchange(AbstractAccount targetAccount, double amount) {
-	    if (this.getBalance() >= amount) {
-	        if (targetAccount instanceof GoldAccountWithInterest) {
-	            double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-	            // Update balances
-	            setBalance(this.getBalance() - amount);
-	            targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-	            System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-	        } else if (targetAccount instanceof RegularAccountWithoutInterest) {
-	            double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-	            // Update balances
-	            setBalance(this.getBalance() - amount);
-	            targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-	            System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-	        } else {
-	            System.out.println("You can only exchange between a gold account with interest (XAU) or a regular account without interest (TRY)!");
-	        }
-	    } else {
-	        System.out.println("Your balance is not enough!");
-	    }
-	}
+	
+    @Override
+    public void exchange(AbstractAccount targetAccount, double amount) {
+    	if (!targetAccount.equals(this)) {
+            if (getBalance() >= amount) {
+                if (isExchangeAllowed(targetAccount)) {
+                    performExchange(targetAccount, amount);
+                } else {
+                    System.out.println("You can only exchange between a gold account (XAU) or a regular account without interest (TRY)!");
+                }
+            } else {
+                System.out.println("Your balance is not enough!");
+            }
+    	}else {
+    		System.out.println("You can't exchange to the account itself, please choose another account!");
+    	}
+    }
+	
+    @Override
+    public void performExchange(AbstractAccount targetAccount, double amount) {
+        double convertedAmount = targetAccount.getBank().convert(getCurrencyType(), targetAccount.getCurrencyType(), amount);
+        // Update balances
+        setBalance(getBalance() - amount);
+        targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
+        System.out.println("Exchanged " + convertedAmount + " " + targetAccount.getCurrencyType().toString() + " to target account!");
+    }
 
+	@Override
+	public boolean isExchangeAllowed(AbstractAccount targetAccount) {
+		return ( (targetAccount instanceof AbstractGoldAccount) || (targetAccount instanceof RegularAccountWithoutInterest)) ;
+	}
 
 }

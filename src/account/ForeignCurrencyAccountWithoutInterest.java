dc8305fe694075a4847063d77ab81e6bc2a3c603
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 14:27:05 +0300

    Update ForeignCurrencyAccountWithoutInterest.java

--- src/account/ForeignCurrencyAccountWithoutInterest.java
+++ src/account/ForeignCurrencyAccountWithoutInterest.java
@@ -19,32 +19,34 @@ public class ForeignCurrencyAccountWithoutInterest extends AbstractForeignCurren
 		System.out.println("- id:" + super.getId() +" Foreign Currency Account Without Interest");
 		
 	}
+
 	
+    @Override
+    public void exchange(AbstractAccount targetAccount, double amount) {
+    	if (!targetAccount.equals(this)) {
+            if (getBalance() >= amount) {
+                if (isExchangeAllowed(targetAccount)) {
+                    performExchange(targetAccount, amount);
+                } else {
+                    System.out.println("You can only exchange with a regular account without interest (TRY) or with a foreign currency account with " + this.getCurrencyType() + " currency!");
+                }
+            } else {
+                System.out.println("Your balance is not enough!");
+            }
+    	}else {
+    		System.out.println("You can't exchange to the account itself, please choose another account!");
+    	}
+    }
+	
+    
 	@Override
-    public void exchange( AbstractAccount targetAccount, double amount) {
-		if (this.getBalance() >= amount) {
-		    if (targetAccount instanceof RegularAccountWithoutInterest) {
-		        double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-		        // Update balances
-		        setBalance(this.getBalance() - amount);
-		        targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-		        System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-		    } else if (targetAccount instanceof ForeignCurrencyAccountWithoutInterest) {
-		        if (((ForeignCurrencyAccountWithoutInterest) targetAccount).getCurrencyType().equals(this.getCurrencyType())) {
-		            double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-		            // Update balances
-		            setBalance(this.getBalance() - amount);
-		            targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-		            System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-		        }
-		        else {
-		        	System.out.println("Selected account's currency type is not " + this.getCurrencyType()+"!");
-		        }
-		    } else {
-		        System.out.println("You can only exchange with a regular account without interest (TRY) or a foreign currency account with interest(" + this.getCurrencyType() + ")!");
-		    }
-		} else {
-		    System.out.println("Your balance is not enough!");
-		}
+	public boolean isExchangeAllowed(AbstractAccount targetAccount) {
+		return ((targetAccount instanceof RegularAccountWithoutInterest) || ((targetAccount instanceof AbstractForeignCurrencyAccount) && (((AbstractForeignCurrencyAccount) targetAccount).getCurrencyType().equals(this.getCurrencyType()))));
+	}
+
+	@Override
+	public void performExchange(AbstractAccount targetAccount, double amount) {
+		// TODO Auto-generated method stub
+		
 	}
 }

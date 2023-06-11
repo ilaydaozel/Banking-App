da1917ab07f1e579815dd09c49860593a34788e4
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 13:18:16 +0300

    Update GoldAccountWithoutInterestMenu.java

--- src/menu/GoldAccountWithoutInterestMenu.java
+++ src/menu/GoldAccountWithoutInterestMenu.java
@@ -1,50 +1,66 @@
 package menu;
 
-import java.util.Scanner;
 
 import account.AbstractAccount;
-import account.GoldAccountWithInterest;
 import account.GoldAccountWithoutInterest;
 import helpers.HelperClient;
 import helpers.HelperIO;
-import helpers.HelperMenu;
+import interfaces.IMenu;
 import user.Client;
 
-public class GoldAccountWithoutInterestMenu  {
+public class GoldAccountWithoutInterestMenu implements IMenu {
+
+    private GoldAccountWithoutInterest goldAccount;
+    private Client client;
 
     public GoldAccountWithoutInterestMenu(GoldAccountWithoutInterest goldAccount, Client client) {
-        HelperIO helperIO = new HelperIO();
-        HelperClient helperClient = new HelperClient(client);
-        boolean exit = false;
-        while (!exit) {
-        	printGoldAccountWithoutInterestMenu ();
-            int choice = helperIO.readIntegerInput();
-
-            switch (choice) {
-            case 1:
-            	System.out.println("Balance: " + goldAccount.getBalance() + " " + goldAccount.getCurrencyType());
-                System.out.println();
-                break;
-            case 2:
-                System.out.println("Select an account to make an exchange with:");
-                AbstractAccount destAccount = helperClient.selectAnAccount();
-                System.out.println("Your current balance: " + goldAccount.getBalance());
-                System.out.println("Enter exchange amount (XAU):");
-                double exchangeAmount = helperIO.readDoubleInput();
-                goldAccount.exchange(destAccount, exchangeAmount);
-                System.out.println();
-                break;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-        }
-        }
+        this.goldAccount = goldAccount;
+        this.client = client;
     }
-    private void printGoldAccountWithoutInterestMenu() {
+
+    @Override
+    public void displayMenu() {
         System.out.println("------ Gold Account (XAU) without Interest Menu ------");
         System.out.println("1. Check Balance");
         System.out.println("2. Exchange");
         System.out.println("0. Go back to main menu");
-
         System.out.print("Enter your choice: ");
+    }
+
+    @Override
+    public void handleChoice() {
+        HelperIO helperIO = new HelperIO();
+        HelperClient helperClient = new HelperClient(client);
+        boolean exit = false;
+        while (!exit) {
+            displayMenu();
+            int choice = helperIO.readIntegerInput();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Balance: " + goldAccount.getBalance() + " " + goldAccount.getCurrencyType());
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("Select an account to make an exchange with:");
+                    AbstractAccount destAccount = helperClient.selectAnAccount();
+                    System.out.println("Your current balance: " + goldAccount.getBalance());
+                    System.out.println("Enter exchange amount (XAU):");
+                    double exchangeAmount = helperIO.readDoubleInput();
+                    goldAccount.exchange(destAccount, exchangeAmount);
+                    System.out.println();
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    @Override
+    public void executeMenu() {
+        handleChoice();
     }
 }

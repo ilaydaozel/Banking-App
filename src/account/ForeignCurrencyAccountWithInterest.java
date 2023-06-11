96681df836066de0afdf53f5a149a61aa41f9919
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 14:27:08 +0300

    Update ForeignCurrencyAccountWithInterest.java

--- src/account/ForeignCurrencyAccountWithInterest.java
+++ src/account/ForeignCurrencyAccountWithInterest.java
@@ -3,89 +3,100 @@ package account;
 import enums.CurrencyType;
 import interfaces.IWithInterest;
 
-public class ForeignCurrencyAccountWithInterest extends AbstractForeignCurrencyAccount implements IWithInterest{
-	private int interestStartDate;
-	
-	public ForeignCurrencyAccountWithInterest(int id, CurrencyType curType) {
-		super(id, curType);
-		// TODO Auto-generated constructor stub
-	}
+public class ForeignCurrencyAccountWithInterest extends AbstractForeignCurrencyAccount implements IWithInterest {
+    private int interestStartDate;
 
-	@Override
-	public void display() {
-		System.out.println("- id:" + super.getId() + " Foreign Currency Account With Interest ");
-		
-	}
+    public ForeignCurrencyAccountWithInterest(int id, CurrencyType curType) {
+        super(id, curType);
+    }
 
-	@Override
-	public void exchange(AbstractAccount targetAccount, double amount) {
-		if(endInterest()) {
-				if (targetAccount instanceof ForeignCurrencyAccountWithoutInterest) {
-			        if (((ForeignCurrencyAccountWithoutInterest) targetAccount).getCurrencyType().equals(this.getCurrencyType())) {
-						if (this.getBalance() >= amount) {
-			            double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-			            // Update balances
-			            setBalance(this.getBalance() - amount);
-			            targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-			            System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-						}
-					 else {
-						    System.out.println("Your balance is not enough!");
-						}
-			        }
-			        else {
-			        	System.out.println("Selected account's currency type is not " + this.getCurrencyType()+"!");
-			        }
-			    } else {
-			        System.out.println("You can only exchange with a foreign currency account without interest(" + this.getCurrencyType() + ")!");
-			    }
-		}
-	}
+    @Override
+    public void display() {
+        System.out.println("- id:" + super.getId() + " Foreign Currency Account With Interest");
+    }
 
-	public int getInterestStartDate() {
-		return interestStartDate;
-	}
+    @Override
+    public void exchange(AbstractAccount targetAccount, double amount) {
+    	if(!targetAccount.equals(this)) {
+    		if (endInterest()) {
+    			if (getBalance() >= amount) {
+        			if (isExchangeAllowed(targetAccount)) {
+                        performExchange(targetAccount, amount);
+                    } else {
+                        System.out.println("You can only exchange with a foreign currency account with " + this.getCurrencyType() + " currency!");
+                    }
+    			}else {
+    				System.out.println("Your balance is not enough!");
+    			}
 
-	public void setInterestStartDate(int interestStartDate) {
-		this.interestStartDate = interestStartDate;
-	}
-	public void resetInterestDay() {
-		setInterestStartDate(getBank().getCurrentDay());
-	}
-	public boolean endInterest() {
-	    double totalDays = getBank().getCurrentDay() - getInterestStartDate();
-	    if (totalDays > 0) {
-	        resetInterestDay();
-	        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-	        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
-	        setBalance(getBalance() + compoundInterest);
-	        System.out.println(getCurrencyType().toString() + " account new balance: " + getBalance());
-	        return true;
-	    } else {
-	        System.out.println("Exchange Failed. 0 days has passed since interest started.");
-	        return false;
-	    }
-	}
+            }
+    	}else {
+    		System.out.println("You can't exchange to the account itself, please choose another account!");
+    	}
+        
+    }
 
-	private double calculateCompoundInterest(double principal, double interestRate, double totalDays) {
-	    double dailyRate = 1.0 + (interestRate / 365.0); // Assuming interest is calculated daily
-	    double compoundInterest = principal * Math.pow(dailyRate, totalDays) - principal;
-	    return compoundInterest;
-	}
-	public double checkExpectedBalance(int days) {
-	    double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-	    double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, days);
-	    double expectedBalance = getBalance() + compoundInterest;
-	    return expectedBalance;
-	}
-	
-	public void updateInterest() {
-		  double totalDays = getBank().getCurrentDay() - getInterestStartDate();
-		    if (totalDays > 0) {
-		        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-		        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);    
-		        setBalance(getBalance() + compoundInterest);
-		    } 
-	}
-	
+    @Override
+    public boolean isExchangeAllowed(AbstractAccount targetAccount) {
+        return (targetAccount instanceof AbstractForeignCurrencyAccount) && ((AbstractForeignCurrencyAccount) targetAccount).getCurrencyType().equals(this.getCurrencyType());
+    }
+
+    @Override
+    public void performExchange(AbstractAccount targetAccount, double amount) {
+        double convertedAmount = getBank().convert(getCurrencyType(), targetAccount.getCurrencyType(), amount);
+        // Update balances
+        setBalance(getBalance() - amount);
+        targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
+        System.out.println("Exchanged " + convertedAmount + " " + targetAccount.getCurrencyType().toString() + " to target account!");
+    }
+
+    public int getInterestStartDate() {
+        return interestStartDate;
+    }
+
+    public void setInterestStartDate(int interestStartDate) {
+        this.interestStartDate = interestStartDate;
+    }
+
+    public void resetInterestDay() {
+        setInterestStartDate(getBank().getCurrentDay());
+    }
+
+    public boolean endInterest() {
+        double totalDays = getBank().getCurrentDay() - getInterestStartDate();
+        if (totalDays > 0) {
+            resetInterestDay();
+            double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+            double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
+            setBalance(getBalance() + compoundInterest);
+            System.out.println(getCurrencyType().toString() + " account new balance: " + getBalance());
+            return true;
+        } else {
+            System.out.println("Exchange Failed. 0 days have passed since interest started.");
+            return false;
+        }
+    }
+
+    private double calculateCompoundInterest(double principal, double interestRate, double totalDays) {
+        double dailyRate = 1.0 + (interestRate / 365.0); // Assuming interest is calculated daily
+        double compoundInterest = principal * Math.pow(dailyRate, totalDays) - principal;
+        return compoundInterest;
+    }
+
+    public double checkExpectedBalance(int days) {
+        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, days);
+        double expectedBalance = getBalance() + compoundInterest;
+        return expectedBalance;
+    }
+
+    public void updateInterest() {
+        double totalDays = getBank().getCurrentDay() - getInterestStartDate();
+        if (totalDays > 0) {
+            double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+            double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
+            setBalance(getBalance() + compoundInterest);
+        }
+    }
 }
+

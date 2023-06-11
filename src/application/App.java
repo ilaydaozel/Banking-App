30c3e8ba04c40db1de901caed10460285db0c9ec
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 14:26:54 +0300

    Update App.java

--- src/application/App.java
+++ src/application/App.java
@@ -1,20 +1,14 @@
 package application;
-
-import java.util.Scanner;
-
-import helpers.HelperIO;
-import menu.BankMenu;
-import menu.ClientMenu;
 import menu.MainMenu;
+import interfaces.IMenu;
 import user.Bank;
-import user.Client;
-import helpers.*;
+
 
 public class App {
     private static Bank bank= Bank.getInstance();
 
 	public static void main(String[] args) {
-		MainMenu mainMenu = new MainMenu(bank);
-        mainMenu.performMainMenu();
+		IMenu mainMenu = new MainMenu(bank);
+        mainMenu.displayMenu();
     }
 }

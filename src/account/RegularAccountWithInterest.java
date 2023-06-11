76e5d3aaa0c41d790d83f73f340ed2a44d24e938
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 12:29:38 +0300

    Update RegularAccountWithInterest.java

--- src/account/RegularAccountWithInterest.java
+++ src/account/RegularAccountWithInterest.java
@@ -3,81 +3,96 @@ package account;
 import enums.CurrencyType;
 import interfaces.IWithInterest;
 
-public class RegularAccountWithInterest extends AbstractRegularAccount implements IWithInterest{
-	private int interestStartDate;
-	public RegularAccountWithInterest(int id) {
-		super(id);
-		// TODO Auto-generated constructor stub
-	}
-	@Override
-	public void display() {
-		System.out.println("- id:" + super.getId() +" Regular Account With Interest");
-		
-	}
-	
-	@Override
-	public void exchange(AbstractAccount targetAccount, double amount) {
-		if(endInterest()) {	
-			if (this.getBalance() >= amount) {
-				if (targetAccount instanceof RegularAccountWithInterest) {
-		            double convertedAmount = this.getBank().convert(this.getCurrencyType(), targetAccount.getCurrencyType(), amount);
-		            // Update balances
-		            setBalance(this.getBalance() - amount);
-		            targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
-		            System.out.println("Exchanged " + convertedAmount + " "+ targetAccount.getCurrencyType().toString() + " to target account!");
-		        
-			    } else {
-			        System.out.println("You can only with a regular account without interest (TRY)!");
-			    }
-			} else {
-			    System.out.println("Your balance is not enough!");
-			}
-		}
-	}
-	public int getInterestStartDate() {
-		return interestStartDate;
-	}
-	public void setInterestStartDate(int interestStartDate) {
-		this.interestStartDate = interestStartDate;
-	}
-	public void resetInterestDay() {
-		setInterestStartDate(getBank().getCurrentDay());
-	}
+public class RegularAccountWithInterest extends AbstractRegularAccount implements IWithInterest {
+    private int interestStartDate;
 
-	public void updateInterest() {
-		  double totalDays = getBank().getCurrentDay() - getInterestStartDate();
-		    if (totalDays > 0) {
-		        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-		        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);    
-		        setBalance(getBalance() + compoundInterest);
-		    } 
-	}
-	public boolean endInterest() {
-	    double totalDays = getBank().getCurrentDay() - getInterestStartDate();
-	    if (totalDays > 0) {
-	        resetInterestDay();
-	        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-	        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
-	        setBalance(getBalance() + compoundInterest);
-	        System.out.println(getCurrencyType().toString() + " account new balance: " + getBalance());
-	        return true;
-	    } else {
-	        System.out.println("0 days has passed since interest started.");
-	        return false;
-	    }
-	}
+    public RegularAccountWithInterest(int id) {
+        super(id);
+    }
 
-	private double calculateCompoundInterest(double principal, double interestRate, double totalDays) {
-	    double dailyRate = 1.0 + (interestRate / 365.0); // Assuming interest is calculated daily
-	    double compoundInterest = principal * Math.pow(dailyRate, totalDays) - principal;
-	    return compoundInterest;
-	}
-	public double checkExpectedBalance(int days) {
-	    double interestRate = getBank().getInterestRate(getCurrencyType().toString());
-	    double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, days);
-	    double expectedBalance = getBalance() + compoundInterest;
-	    return expectedBalance;
-	}
+    @Override
+    public void display() {
+        System.out.println("- id: " + super.getId() + " Regular Account With Interest");
+    }
 
+    @Override
+    public void exchange(AbstractAccount targetAccount, double amount) {
+    	if (!targetAccount.equals(this)) {
+    		if (endInterest()) {
+                if (getBalance() >= amount) {
+                    if (isExchangeAllowed(targetAccount)) {
+                        performExchange(targetAccount, amount);
+                    } else {
+                        System.out.println("You can only exchange with a regular account without interest (TRY)!");
+                    }
+                } else {
+                    System.out.println("Your balance is not enough!");
+                }
+            }
+    	}else {
+    		System.out.println("You can't exchange to the account itself, please choose another account!");
+    	}
+        
+    }
+
+    private boolean isExchangeAllowed(AbstractAccount targetAccount) {
+        return targetAccount instanceof RegularAccountWithoutInterest;
+    }
+
+    private void performExchange(AbstractAccount targetAccount, double amount) {
+        double convertedAmount = getBank().convert(getCurrencyType(), targetAccount.getCurrencyType(), amount);
+        // Update balances
+        setBalance(getBalance() - amount);
+        targetAccount.setBalance(targetAccount.getBalance() + convertedAmount);
+        System.out.println("Exchanged " + convertedAmount + " " + targetAccount.getCurrencyType().toString() + " to target account!");
+    }
+
+    public int getInterestStartDate() {
+        return interestStartDate;
+    }
+
+    public void setInterestStartDate(int interestStartDate) {
+        this.interestStartDate = interestStartDate;
+    }
+
+    public void resetInterestDay() {
+        setInterestStartDate(getBank().getCurrentDay());
+    }
+
+    public void updateInterest() {
+        double totalDays = getBank().getCurrentDay() - getInterestStartDate();
+        if (totalDays > 0) {
+            double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+            double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
+            setBalance(getBalance() + compoundInterest);
+        }
+    }
+
+    public boolean endInterest() {
+        double totalDays = getBank().getCurrentDay() - getInterestStartDate();
+        if (totalDays > 0) {
+            resetInterestDay();
+            double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+            double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, totalDays);
+            setBalance(getBalance() + compoundInterest);
+            System.out.println(getCurrencyType().toString() + " account new balance: " + getBalance());
+            return true;
+        } else {
+            System.out.println("0 days have passed since interest started.");
+            return false;
+        }
+    }
+
+    private double calculateCompoundInterest(double principal, double interestRate, double totalDays) {
+        double dailyRate = 1.0 + (interestRate / 365.0); // Assuming interest is calculated daily
+        double compoundInterest = principal * Math.pow(dailyRate, totalDays) - principal;
+        return compoundInterest;
+    }
+
+    public double checkExpectedBalance(int days) {
+        double interestRate = getBank().getInterestRate(getCurrencyType().toString());
+        double compoundInterest = calculateCompoundInterest(getBalance(), interestRate, days);
+        double expectedBalance = getBalance() + compoundInterest;
+        return expectedBalance;
+    }
 }
-

e00d99853317cde2960852b597c7c50be5a120a2
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 12:47:06 +0300

    Implement IMenu in GoldAccountWithInterestMenu

--- src/menu/GoldAccountWithInterestMenu.java
+++ src/menu/GoldAccountWithInterestMenu.java
@@ -4,57 +4,74 @@ import account.AbstractAccount;
 import account.GoldAccountWithInterest;
 import helpers.HelperClient;
 import helpers.HelperIO;
+import interfaces.IMenu;
 import user.Client;
 
-public class GoldAccountWithInterestMenu  {
-	
-    public GoldAccountWithInterestMenu (GoldAccountWithInterest goldAccount, Client client) {
-        HelperIO helperIO = new HelperIO();
-        HelperClient helperClient = new HelperClient(client);
-        boolean exit = false;
-        while (!exit) {
-        	printGoldAccountWithInterestMenu ();
-            int choice = helperIO.readIntegerInput();
+public class GoldAccountWithInterestMenu implements IMenu {
 
-            switch (choice) {
-            case 1:
-                System.out.println("Balance: " + goldAccount.getBalance() + " " + goldAccount.getCurrencyType());
-                System.out.println();
-                break;
-            case 2:
-                System.out.println("Select an account to make an exchange with:");
-                AbstractAccount destAccount = helperClient.selectAnAccount();
-                System.out.println("Your current balance: " + goldAccount.getBalance());
-                System.out.println("Enter exchange amount (XAU):");
-                double exchangeAmount = helperIO.readDoubleInput();
-                goldAccount.exchange(destAccount, exchangeAmount);
-                System.out.println();
-                break;
-            case 3:
-                System.out.println("Enter a day amount to calculate the expected balance:");
-                int dayAmount = helperIO.readIntegerInput();
-                double expectedAmount = goldAccount.checkExpectedBalance(dayAmount);
-                System.out.println("The balance is expected to be " + expectedAmount + " " + goldAccount.getCurrencyType() + " after " + dayAmount + " days." );
-                System.out.println();
-                break;    
-            case 0:
-                // Go back to the main menu
-                break;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-        }
-        }
+    private GoldAccountWithInterest account;
+    private Client client;
+    private HelperIO helperIO = new HelperIO();
+
+    public GoldAccountWithInterestMenu(GoldAccountWithInterest account, Client client) {
+        this.account = account;
+        this.client = client;
     }
 
-    private void printGoldAccountWithInterestMenu() {
+    @Override
+    public void displayMenu() {
         System.out.println("------ Gold Account (XAU) with Interest Menu ------");
         System.out.println("1. Check Balance");
         System.out.println("2. Exchange to another account (XAU with Interest)");
         System.out.println("3. See expected balance");
         System.out.println("0. Go back to main menu");
-
         System.out.print("Enter your choice: ");
+    }
+
+    @Override
+    public void handleChoice() {
+        HelperClient helperClient = new HelperClient(client);
+        
+        boolean exit = false;
+        while (!exit) {
+        	displayMenu();
+        	int choice = helperIO.readIntegerInput();
+	        switch (choice) {
+	            case 1:
+	                System.out.println("Balance: " + account.getBalance() + " " + account.getCurrencyType());
+	                System.out.println();
+	                break;
+	            case 2:
+	                System.out.println("Select an account to make an exchange with:");
+	                AbstractAccount destAccount = helperClient.selectAnAccount();
+	                System.out.println("Your current balance: " + account.getBalance());
+	                System.out.println("Enter exchange amount (XAU):");
+	                double exchangeAmount = helperIO.readDoubleInput();
+	                account.exchange(destAccount, exchangeAmount);
+	                System.out.println();
+	                break;
+	            case 3:
+	                System.out.println("Enter a day amount to calculate the expected balance:");
+	                int dayAmount = helperIO.readIntegerInput();
+	                double expectedAmount = account.checkExpectedBalance(dayAmount);
+	                System.out.println("The balance is expected to be " + expectedAmount + " " + account.getCurrencyType() + " after " + dayAmount + " days.");
+	                System.out.println();
+	                break;
+	            case 0:
+	            	exit = true;
+	            	System.out.println();
+	                // Go back to the main menu
+	                break;
+	            default:
+	                System.out.println("Invalid choice. Please try again.");
+	        }
         }
-    
+        	
+    }
+
+    @Override
+    public void executeMenu() {
+        handleChoice();
+    }
 }
 

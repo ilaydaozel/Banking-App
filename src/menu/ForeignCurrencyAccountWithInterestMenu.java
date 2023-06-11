fd74c43dea7443b08c4948f16e6027b39ebb31bc
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 13:18:24 +0300

    Update ForeignCurrencyAccountWithInterestMenu.java

--- src/menu/ForeignCurrencyAccountWithInterestMenu.java
+++ src/menu/ForeignCurrencyAccountWithInterestMenu.java
@@ -4,58 +4,73 @@ import account.AbstractAccount;
 import account.ForeignCurrencyAccountWithInterest;
 import helpers.HelperClient;
 import helpers.HelperIO;
+import interfaces.IMenu;
 import user.Client;
 
-public class ForeignCurrencyAccountWithInterestMenu {
+public class ForeignCurrencyAccountWithInterestMenu implements IMenu {
 
-    public ForeignCurrencyAccountWithInterestMenu(ForeignCurrencyAccountWithInterest foreignAccount, Client client) {
+    private ForeignCurrencyAccountWithInterest account;
+    private Client client;
+    private HelperIO helperIO = new HelperIO();
+
+    public ForeignCurrencyAccountWithInterestMenu(ForeignCurrencyAccountWithInterest account, Client client) {
+        this.account = account;
+        this.client = client;
+    }
+
+    @Override
+    public void displayMenu() {
+        System.out.println("------ Foreign Currency Account with Interest Menu ------");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Exchange");
+        System.out.println("3. See expected balance");
+        System.out.println("0. Go back to main menu");
+        System.out.print("Enter your choice: ");
+    }
+
+    @Override
+    public void handleChoice() {
         HelperClient helperClient = new HelperClient(client);
-        HelperIO helperIO = new HelperIO();
 
         boolean exit = false;
         while (!exit) {
-            printForeignCurrencyAccountWithInterestMenu();
+            displayMenu();
             int choice = helperIO.readIntegerInput();
-
             switch (choice) {
                 case 1:
-                    System.out.println("Balance: " + foreignAccount.getBalance()+ " " + foreignAccount.getCurrencyType());
+                    System.out.println("Balance: " + account.getBalance() + " " + account.getCurrencyType());
                     System.out.println();
                     break;
                 case 2:
                     System.out.println("Select an account to make an exchange with:");
-                    AbstractAccount destAccount = helperClient.selectAnAccount();
-                    System.out.println("Your current balance: " + foreignAccount.getBalance());
-                    System.out.println("Enter exchange amount (" + foreignAccount.getCurrencyType() +") :");
+                    AbstractAccount targetAccount = helperClient.selectAnAccount();
+                    System.out.println("Your current balance: " + account.getBalance());
+                    System.out.println("Enter exchange amount (" + account.getCurrencyType() + "):");
                     double exchangeAmount = helperIO.readDoubleInput();
-                    foreignAccount.exchange(destAccount, exchangeAmount);
+                    account.exchange(targetAccount, exchangeAmount);
                     System.out.println();
                     break;
                 case 3:
                     System.out.println("Enter a day amount to calculate the expected balance:");
                     int dayAmount = helperIO.readIntegerInput();
-                    double expectedAmount = foreignAccount.checkExpectedBalance(dayAmount);
-                    System.out.println("The balance is expected to be " + expectedAmount + " " + foreignAccount.getCurrencyType() + " after " + dayAmount + " days." );
+                    double expectedAmount = account.checkExpectedBalance(dayAmount);
+                    System.out.println("The balance is expected to be " + expectedAmount + " " + account.getCurrencyType() + " after " + dayAmount + " days.");
                     System.out.println();
-                    break; 
+                    break;
                 case 0:
-                    // Go back to the main menu
                     exit = true;
+                    System.out.println();
+                    // Go back to the main menu
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
         }
     }
-    
-    private void printForeignCurrencyAccountWithInterestMenu() {
-        System.out.println("------ Foreign Currency Account (USD) with Interest Menu ------");
-        System.out.println("1. Check Balance");
-        System.out.println("2. Exchange");
-        System.out.println("3. See expected balance");
-        System.out.println("0. Go back to main menu");
 
-        System.out.print("Enter your choice: ");
+    @Override
+    public void executeMenu() {
+        handleChoice();
     }
-
 }
+

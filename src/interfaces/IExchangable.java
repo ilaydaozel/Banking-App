4122679ccc5c71c9e1902a3cd33f946a8a0701e2
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 14:26:46 +0300

    Update IExchangable.java

--- src/interfaces/IExchangable.java
+++ src/interfaces/IExchangable.java
@@ -4,4 +4,8 @@ import account.AbstractAccount;
 
 public interface IExchangable {
 	public void exchange( AbstractAccount targetAccount, double amount);
+	public boolean isExchangeAllowed(AbstractAccount targetAccount);
+	public void performExchange(AbstractAccount targetAccount, double amount);
+	
+	
 }

924f436d0512c5c67f912668c439e8bc3f6d5d4f
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 11:47:51 +0300

    Implement IMenu in RegularAccountWithInterestMenu

--- src/menu/RegularAccountWithInterestMenu.java
+++ src/menu/RegularAccountWithInterestMenu.java
@@ -4,50 +4,21 @@ import account.AbstractAccount;
 import account.RegularAccountWithInterest;
 import helpers.HelperClient;
 import helpers.HelperIO;
+import interfaces.IMenu;
 import user.Client;
 
-public class RegularAccountWithInterestMenu  {
+public class RegularAccountWithInterestMenu implements IMenu {
+
+    private RegularAccountWithInterest account;
+    private Client client;
 
     public RegularAccountWithInterestMenu(RegularAccountWithInterest account, Client client) {
-    	HelperClient helperClient = new HelperClient(client);
-        HelperIO helperIO = new HelperIO();
-        
-        boolean exit = false;
-        while (!exit) {
-           printRegularAccountTRYWithInterestMenu ();
-            int choice = helperIO.readIntegerInput();
-
-            switch (choice) {
-                case 1:
-                	System.out.println("Balance:" + account.getBalance() + " " + account.getCurrencyType());
-                	System.out.println();
-                    break;
-                case 2:
-                	System.out.println("Select a regular account without interest to make exchange to:");
-                	AbstractAccount destAccount = helperClient.selectAnAccount();
-                	System.out.println("Your current balance: " + account.getBalance());
-                    System.out.println("Enter exchange amount (TRY):");
-                    double exchangeAmount = helperIO.readDoubleInput();
-                	account.exchange(destAccount, exchangeAmount);
-                	System.out.println();
-                    break;
-                case 3:
-                    System.out.println("Enter a day amount to calculate the expected balance:");
-                    int dayAmount = helperIO.readIntegerInput();
-                    double expectedAmount = account.checkExpectedBalance(dayAmount);
-                    System.out.println("The balance is expected to be " + expectedAmount + " " + account.getCurrencyType() + " after " + dayAmount + " days." );
-                    System.out.println();
-                    break; 
-                case 0:
-                    // Go back to the main menu
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
+        this.account = account;
+        this.client = client;
     }
-    private void printRegularAccountTRYWithInterestMenu() {
+
+    @Override
+    public void displayMenu() {
         System.out.println("------ Regular Account (TRY) with Interest Menu ------");
         System.out.println("1. Check Balance");
         System.out.println("2. Exchange");
@@ -55,6 +26,50 @@ public class RegularAccountWithInterestMenu  {
         System.out.println("0. Go back to main menu");
         System.out.print("Enter your choice: ");
     }
+
+    @Override
+    public void handleChoice(int choice) {
+        HelperClient helperClient = new HelperClient(client);
+        HelperIO helperIO = new HelperIO();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Balance: " + account.getBalance() + " " + account.getCurrencyType());
+                System.out.println();
+                break;
+            case 2:
+                System.out.println("Select an account without interest to make an exchange to:");
+                AbstractAccount destAccount = helperClient.selectAnAccount();
+                System.out.println("Your current balance: " + account.getBalance());
+                System.out.println("Enter exchange amount (TRY):");
+                double exchangeAmount = helperIO.readDoubleInput();
+                account.exchange(destAccount, exchangeAmount);
+                System.out.println();
+                break;
+            case 3:
+                System.out.println("Enter a day amount to calculate the expected balance:");
+                int dayAmount = helperIO.readIntegerInput();
+                double expectedAmount = account.checkExpectedBalance(dayAmount);
+                System.out.println("The balance is expected to be " + expectedAmount + " " + account.getCurrencyType() + " after " + dayAmount + " days.");
+                System.out.println();
+                break;
+            case 0:
+                // Go back to the main menu
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+        }
+    }
     
+    @Override
+    public void executeMenu() {
+        boolean exit = false;
+        while (!exit) {
+            displayMenu();
+            int choice = helperIO.readIntegerInput();
+            handleChoice(choice);
+        }
+    }
 }
+
 
